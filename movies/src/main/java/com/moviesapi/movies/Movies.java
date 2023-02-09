86c9e65a45516d86ab1d948daa941971f122d95b
Deepthi <deeptijnair@gmail.com>
2023-02-08 22:29:17 -0500

    Update Movies.java

--- movies/src/main/java/com/moviesapi/movies/Movies.java
+++ movies/src/main/java/com/moviesapi/movies/Movies.java
@@ -1,9 +1,12 @@
 package com.moviesapi.movies;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -11,7 +14,12 @@ import java.util.List;
 // It also allows us to choose the name of the collection we want to use
 //Note, that this annotation is the Mongo equivalent of @Entity in JPA.
 @Document(collection = "movies")
+//Takes care of getters,setters ,toString methods, constructors
 @Data
+//Annotation for constructor that takes all the private fields below as arguments
+@AllArgsConstructor
+//Constructor that does not take any arguments
+@NoArgsConstructor
 public class Movies {
 
     @Id
@@ -23,5 +31,7 @@ public class Movies {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference
+    private List<String> reviewsId;
 
 }
