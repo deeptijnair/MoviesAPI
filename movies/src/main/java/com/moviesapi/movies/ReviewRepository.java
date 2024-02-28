be4a081ff8e632987992301d15e68dafbc7b1539
Deepthi <deeptijnair@gmail.com>
2024-02-28 13:31:28 -0500

    Added Review related classes and modified other files

--- movies/src/main/java/com/moviesapi/movies/ReviewRepository.java
+++ movies/src/main/java/com/moviesapi/movies/ReviewRepository.java
@@ -1,4 +1,9 @@
 package com.moviesapi.movies;
 
-public class ReviewRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
---
+++ movies/src/main/java/com/moviesapi/movies/ReviewService.java
@@ -0,0 +1,31 @@
+package com.moviesapi.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId){
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+
+        mongoTemplate.update(Movies.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value("review"))
+                .first();
+        return review;
+    }
+}
